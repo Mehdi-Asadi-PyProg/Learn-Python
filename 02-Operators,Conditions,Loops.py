c0491588c27bf9826e8b1cb67d71268652320782
m.asadi <mehdi.asadi004@gmail.com>
2026-07-20 14:48:12 +0330

    Opertors added

---
+++ 02-Operators,Conditions,Loops.py
@@ -0,0 +1,40 @@
+# Arithmathic Operation
+a = 17
+b = 5
+
+add_result = a + b          # Addition
+sub_result = a - b          # Subtraction
+Mult_result = a * b         # Multiplication
+div_result = a / b          # Devision
+print(f"Divesion Operation {div_result}")
+floor_div_Result = a // b   # Floor Devision
+print(f"Floor Devision Operation {floor_div_Result}")
+modulus_Result = a % b      # Modulus
+print(f"Modulus Operation {modulus_Result}")
+exponent_result = a ** b
+print(f"Exponent Operation {exponent_result}")
+
+# Comarssion Operators
+a = 10
+b = 10
+print("-" * 50)
+print(f"{a} == {b} ",a == b)
+num1 = 36
+num2 = 27
+print(f"{num1} != {num2} ", num1 != num2)
+print(f"{num1} <= {num2} ",num1 <= num2)
+print(f"{num1} > {num2} ",num1 > num2)
+
+str1 = "Hello"
+str2 = "Bye"
+print(f"{str1} == {str2}",str1 == str2)
+
+# Logical Operations  AND OR NOT
+print("-" * 50)
+
+x = True
+y = False
+
+print(f"{x} and {y}", x and y)
+print(f"{x} or {y} ",x or y)
+print(f"not {y} ",not y)
