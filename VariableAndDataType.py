c78e794fd656871352ba21500d841e10dfa66a56
m.asadi <mehdi.asadi004@gmail.com>
2026-07-20 10:17:01 +0330

    VariableAndDatatype file is updated

--- VariableAndDataType.py
+++ VariableAndDataType.py
@@ -1,1 +1,26 @@
-print("Hello World")
+# Declaring Variables
+age:int = 38
+height: float = 6.1
+name: str = "Mehdi"
+is_student:bool = True
+print(f"My name is {name}, I'm {age} Years Old and {height} inches height")
+
+# Print Type of Variables
+print(type(name))
+
+
+# Type Convertion
+cost = "250"
+print(f"cost variable type is {type(cost)}")
+int_cost = int(cost)
+print(f"int_cost variable type is {type(int_cost)}")
+
+# Dynamic Variable
+var = 10
+var = 15.342
+var = "Mehdi"
+print(var)
+
+# Input Values 
+age = input("what is the age? ")
+print(f"Your age is : {age}")
